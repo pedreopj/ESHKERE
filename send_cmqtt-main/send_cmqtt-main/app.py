04bcbc3c2bc6232decdc1125f1d7fc0f3f0f868c
pedreopj <pelaezescobarpepo@gmail.com>
2024-09-19 06:52:15 -0500

    Update app.py

--- send_cmqtt-main/send_cmqtt-main/app.py
+++ send_cmqtt-main/send_cmqtt-main/app.py
@@ -20,7 +20,7 @@ def on_message(client, userdata, message):
 
 broker="broker.mqttdashboard.com"
 port=1883
-client1= paho.Client("GIT-HUB")
+client1= paho.Client("elmar")
 client1.on_message = on_message
 
 
